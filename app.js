dfdcada5d5504ce61d0724cea7911716592b2e28
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@CFP127ALUNO.LOCAL>
2025-10-20 17:02:36 -0300

    Criação dados Artistas no app

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 'use strick'
+
+
+
+async function buscarTituloMusica (nome) {
+    const nomeCodificado = encodeURIComponent(nome.trim());
+
+    const URL = `https://musicbrainz.org/ws/2/artist/?query=${nomeCodificado}&fmt=JSON`;
+
+    const response = await fetch(URL);
+
+    const data = await response.json();
+    return data.artist || [];
+    
+}
+
+async function buscarArtista() {
+    const inputArtist = document.getElementById('buscar-artista');
+    const namePesquisado = inputArtist.value;
+    const listArtist = document.getElementById('lista')
+}
+
+buscarTituloMusica("Madonna");
