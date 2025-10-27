88c86eb8fa21ddbef76e85e06f2d1bd45ceb7165
MATHEUS GABRIEL PEREZ DOS SANTOS <25132972@CFP127ALUNO.LOCAL>
2025-10-27 17:03:10 -0300

    Script JavaScript

--- app.js
+++ app.js
@@ -1,32 +1,54 @@
-'use strick'
+'use strict'
 
 
 
-async function buscarTituloMusica (nome) {
+async function buscarArtistaMusica(nome) {
     const nomeCodificado = encodeURIComponent(nome.trim());
-
-    const URL = `https://musicbrainz.org/ws/2/artist/?query=${nomeCodificado}&fmt=JSON`;
-
-    const response = await fetch(URL);
-
-    const data = await response.json();
-    return data.artist || [];
+    const URL = `https://musicbrainz.org/ws/2/artist/?query=${nomeCodificado}&fmt=json`;
+    const response = await fetch(URL); //Faz a requisição HTTP GET para a URL construída.
     
+    const formatJson = await response.json()
+    console.log(formatJson)
+    
+    return formatJson.artist;
 }
 
 async function buscarArtista() {
-    const inputArtist = document.getElementById('buscar-artista');
-    const nomePesquisado = inputArtist.value;
-    const listArtist = document.getElementById('lista');
+    const inputArtist = document.getElementById('buscar-artista').value.trim()
+    const listaResultados = document.getElementById('catalogo')
+    // console.log(inputArtist)
 
-    if(!nomePesquisado){
-        listArtist = '<p style="color: red;">Por favor, digite um nome de artista.</p>'
-        return;
+    const buscar = await buscarArtistaMusica(inputArtist)
+
+    console.log(typeof buscar)
+
+    if((buscar)){ //Array.isArray() => Serve para garantir que o "Buscar" deve ser um Array antes de chamar o ForEach
+        buscar.forEach((item) => {
+
+           // const nomeMusica = document.createElement('h1')
+
+            const containerArtista = document.createElement('div')
+            containerArtista.classList.add('lista');
+
+            listaResultados.appendChild(containerArtista);
+
+
+        });
     }
 
-    const artistas = await buscarTituloMusica(nomePesquisado)
+    // console.log(buscar)
+    // const nomePesquisado = inputArtist.value;
+    // const listArtist = document.createElement('lista');
+    // listArtist.classList.add('lista')
 
-    exibirResultadoArtista(artistas, cardElement);
+    // if (nomePesquisado) {
+    //     const artistas = await buscarTituloMusica(nomePesquisado)
+    //     listArtist = artistas;
+    //     return listArtist;
+
+    // } else {
+    //     listArtist = '<p style="color: red;">Por favor, digite um nome de artista.</p>'
+    //     return;
+    // }
+
 }
-
-buscarTituloMusica("Madonna");
